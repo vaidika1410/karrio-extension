c4a17c90c49132318b8407fa5213f01eb67a316f
vaidika1410 <vaidikakaul20@gmail.com>
2026-05-21 13:03:06 +0530

    feat: integrate frontend authentication flow

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: "http://localhost:3001",
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   
   await app.listen(process.env.PORT ?? 3000);
