fc430d5e83708a8482b6ec3b6a238537f72d4503
vaidika1410 <vaidikakaul20@gmail.com>
2026-05-31 18:54:47 +0530

    fix: extension packaging

--- apps/extension/package-dist.js
+++ apps/extension/package-dist.js
@@ -1,35 +1,40 @@
-const fs = require('fs');
-const path = require('path');
-const archiver = require('archiver');
+const fs = require("fs");
+const path = require("path");
+const archiver = require("archiver");
 
-const output = fs.createWriteStream(path.join(__dirname, 'karrio-extension-dist.zip'));
-const archive = archiver('zip', { zlib: { level: 9 } });
+const output = fs.createWriteStream(
+  path.join(__dirname, "karrio-extension-dist.zip")
+);
+
+const archive = archiver("zip", {
+  zlib: { level: 9 },
+});
 
 archive.pipe(output);
 
-// Exclude build artifacts and unnecessary files
 const excluded = [
-    'node_modules/**',
-    '.git/**',
-    '.plasmo/**',
-    '.turbo/**',
-    'build/**',
-    'karrio-extension-dist.zip',
-    'package-dist.js'
+  "node_modules/**",
+  ".git/**",
+  ".plasmo/**",
+  ".turbo/**",
+  "build/**",
+  "karrio-extension-dist.zip",
+  "package-dist.js",
 ];
 
-archive.glob('**/*', {
-    cwd: __dirname,
-    ignore: excluded
+archive.glob("**/*", {
+  cwd: __dirname,
+  ignore: excluded,
+});
+
+output.on("close", () => {
+  console.log(
+    `Created karrio-extension-dist.zip (${archive.pointer()} bytes)`
+  );
+});
+
+archive.on("error", (err) => {
+  throw err;
 });
 
 archive.finalize();
-
-output.on('close', function () {
-    console.log(archive.pointer() + ' total bytes');
-    console.log('Zip generated successfully.');
-});
-
-archive.on('error', function (err) {
-    throw err;
-});
